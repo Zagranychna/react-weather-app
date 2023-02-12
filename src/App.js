2bce6d1dc7a4d208a1d7e563a6a59cda9d68d536
Zagranychna <zagranichnaya@gmail.com>
2023-02-12 09:13:02 +0100

    adding boostrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App</h1>
+      <div className="container">
+        <button className="btn btn-primary">Hello</button>
 
-      <footer>
-        This project was coded by Oksana Zagranychna and is{" "}
-        <a
-          href="https://github.com/Zagranychna/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Oksana Zagranychna and is{" "}
+          <a
+            href="https://github.com/Zagranychna/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
